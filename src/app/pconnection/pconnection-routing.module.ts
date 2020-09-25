a4e4047e688979c41c6ed88ab74cc8cef0bed808
Oleg Chirukhin <oleg@jdk.dev>
2020-09-25 15:07:06 +0300

    password

--- src/app/pconnection/pconnection-routing.module.ts
+++ src/app/pconnection/pconnection-routing.module.ts
@@ -1,14 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {DetailsComponent} from "./details/details.component";
 import {CreateComponent} from "./create/create.component";
-import {UpdateComponent} from "./update/update.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'pconnection/home', pathMatch: 'full'},
   {path: 'pconnection/home', component: HomeComponent},
-  {path: 'pconnection/details/:pconnectionId', component: DetailsComponent},
   {path: 'pconnection/create', component: CreateComponent},
   {path: 'pconnection/create/:pconnectionId', component: CreateComponent}
 ];
